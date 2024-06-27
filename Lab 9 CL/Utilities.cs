43f57e30de8bfafa54398b62dcde7a77a96f31fd
Eweaa <88718116+Eweaa@users.noreply.github.com>
2024-06-28 00:10:55 +0300

    Add project files.

---
+++ Lab 9 CL/Utilities.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab_9_CL
+{
+    public class Utilities
+    {
+        static public void Swap<T>(ref T T1, ref T T2)
+        {
+            T temp = T2;
+            T2 = T1;
+            T1 = temp;
+        }
+    }
+
+    public class Point : IComparable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x; Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point p = obj as Point;
+            if (p.X > X) return -1;
+            else if (p.X < X) return 1;
+            else return 0;
+        }
+    }
+
+
+    public interface ISerise
+    {
+        int Next();
+    }
+
+    public class EvenSeries : ISerise
+    {
+        int Num = 0;
+        public int Next()
+        {
+            return Num += 2;
+        }
+    }
+
+    public class OddSeries : ISerise
+    {
+        int Num = 1;
+        public int Next()
+        {
+            return Num += 2;
+        }
+    }
+
+    public class FibonacciSeries : ISerise
+    {
+        int current = 0;
+        int prev = 0;
+        public int Next()
+        {
+            if (current == 0)
+            {
+                current = 1;
+                return current;
+            }
+
+            int nextNum = current + prev;
+            prev = current;
+            current = nextNum;
+
+
+            return nextNum;
+        }
+    }
+
+    public class SeriesEngine
+    {
+        public void PrintNextNumber(ISerise o)
+        {
+            Console.WriteLine(o.Next());
+        }
+    }
+
+}
---
+++ Lab 9/Program.cs
@@ -0,0 +1,51 @@
+﻿using Lab_9_CL;
+namespace Lab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point p1 = new(10, 20);
+            Point p2 = new(50, 20);
+            Point p3 = new(5, 20);
+            Point p4 = new(3, 20);
+            Point[] arr = [p1, p2, p3, p4];
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            int x = 5;
+            int y = 50;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Utilities.Swap(ref x, ref y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            EvenSeries e = new EvenSeries();
+            SeriesEngine engine = new SeriesEngine();
+            OddSeries o = new OddSeries();
+            engine.PrintNextNumber(e);
+            engine.PrintNextNumber(e);
+            engine.PrintNextNumber(e);
+            Console.WriteLine("----------------");
+            engine.PrintNextNumber(o);
+            engine.PrintNextNumber(o);
+            engine.PrintNextNumber(o);
+            Console.WriteLine("----------------");
+            FibonacciSeries f = new FibonacciSeries();
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+            engine.PrintNextNumber(f);
+        }
+    }
+}
